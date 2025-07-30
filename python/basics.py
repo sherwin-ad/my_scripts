e4644f418dcdd009232f99c623ea8d8afa9c11a2
sherwin-ad <sherwin_ad@yahoo.com>
2025-07-30 18:21:46 +0800

    added python folder

---
+++ python/basics.py
@@ -0,0 +1,25 @@
+import datetime
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+print("Current Date and Time:", current_datetime)
+
+# Get today's date
+today_date = datetime.date.today()
+print("Today's Date:", today_date)
+
+# Create a specific date
+specific_date = datetime.date(2025, 7, 30)
+print("Specific Date:", specific_date)
+
+# Calculate the difference between two dates
+date1 = datetime.date(2025, 7, 30)
+date2 = datetime.date(2025, 8, 15)
+difference = date2 - date1
+print("Difference between dates:", difference.days, "days")
+
+# Format the date
+formatted_date = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+print("Formatted Date and Time:", formatted_date)
+
+print(24 * 7)
